0597f61f35fcda70599a9a03e046ec508f8f0c1c
Ratik Sharma <Ratik96@gmail.com>
2018-08-07 20:05:06 +0530

    Build the layout

--- app/src/main/java/com/ratik/tipcalculator/view/MainActivity.kt
+++ app/src/main/java/com/ratik/tipcalculator/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ratik.tipcalculator
+package com.ratik.tipcalculator.view
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ratik.tipcalculator.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
